da06bae4549fd814d8aab273e31a11d2863afb84
Bruna Moraes <brunacm@id.uff.br>
2023-11-16 00:41:39 -0300

    fix(smart-bank): fix some bugs in handleLogin

--- src/js/login-script.js
+++ src/js/login-script.js
@@ -1,32 +1,23 @@
+const loginForm = document.getElementById('loginForm')
+const userTypeMessage = document.getElementById('userTypeMessage')
+const userType = document.getElementById('userType')
+const loginEmail = document.getElementById('loginEmail').value
+
 function showLoginForm(userType) {
-    const loginForm = document.getElementById("loginForm")
-    const userTypeMessage = document.getElementById("userTypeMessage")
+  userTypeMessage.textContent =
+    userType === 'cliente' ? 'Login como Cliente' : 'Login como Admin'
 
-    userTypeMessage.textContent =
-      userType === "cliente" ? "Login como Cliente" : "Login como Admin"
+  loginForm.classList.remove('d-none')
+}
 
-    loginForm.classList.remove("d-none")
-    console.log("Usuário selecionado:", userType)
-  }
+function hideLoginForm() {
+  loginForm.classList.add('d-none')
+}
 
-  function hideLoginForm() {
-    const loginForm = document.getElementById("loginForm")
-    loginForm.classList.add("d-none")
-  }
+function handleLogin() {
+  userTypeMessage.textContent === 'Login como Cliente'
+    ? (window.location.href = '/public/login-client.html')
+    : (window.location.href = '/public/login-admin.html')
 
-  function handleLogin() {
-    const userType = document
-      .getElementById("loginForm")
-      .classList.contains("d-none")
-      ? ""
-      : "cliente" // Definir o tipo de usuário com base na visibilidade do formulário
-    const loginEmail = document.getElementById("loginEmail").value
-
-    // Lógica para redirecionar com base no tipo de usuário
-    if (userType === "admin") {
-      window.location.href = "/public/login-admin.html"
-    } else if (userType === "cliente") {
-      window.location.href = "/public/login-client.html"
-    }
-    return false
-  }
+  return false
+}
