c2126b2ffe2e4493add8d07cc19bc85cb99a596b
Henrique Santana <henrique.santana@injunior.com.br>
2023-11-18 16:26:12 -0300

    feat (Usuario) getters e setters

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Usuario {
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private int tipo;
+	private String cpf;
+	private String senha;
+	
+	public Usuario(String nome, String sobrenome, String email, int tipo, String cpf, String senha) {
+		
+	}
+
+}
