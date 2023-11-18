b2822e0a4d09bae5fe5a08db82b7659a3b81281a
Henrique Santana <henrique.santana@injunior.com.br>
2023-11-18 19:24:04 -0300

    feat(Transferencia): getters e setters

---
+++ src/main/java/model/Transferencia.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Transferencia {
+	private int	id;
+	private String codigo_rementente;
+	private String codigo_destinatario;
+	private float valor;
+
+	public Transferencia() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
