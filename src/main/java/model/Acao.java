62e3c142fbbe77e6033a7aeb0320ea8c86912da0
Henrique Santana <henrique.santana@injunior.com.br>
2023-11-19 01:03:11 -0300

    feat (Acao): getters e setters

---
+++ src/main/java/model/Acao.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Acao {
+	private String nome;
+	private String descricao;
+	private float preco_unitario;
+
+	public Acao() {
+	}
+	
+	public Acao(String nome, String descricao, float preco_unitario) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco_unitario = preco_unitario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPreco_unitario() {
+		return preco_unitario;
+	}
+
+	public void setPreco_unitario(float preco_unitario) {
+		this.preco_unitario = preco_unitario;
+	}
+}
