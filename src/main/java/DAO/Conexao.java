d8b2dd4572ea70b441914f5a7799e38b51ad4177
Henrique Santana <henrique.santana@injunior.com.br>
2023-11-20 20:09:15 -0300

    feat: nova estrutura de conexão com o db

---
+++ src/main/java/DAO/Conexao.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexao {
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3307/sistemaBancario?useTimeZone=true&serverTimeZone=UTC";
+	private String user = "root";
+	private String password = "12345678";
+	private Connection conexao = null;
+	
+	public Conexao() {
+		try {
+			Class.forName(driver);
+			conexao = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConexao() {
+		return this.conexao;
+	}
+	
+	public void closeConexao() {
+		try {
+			this.conexao.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
