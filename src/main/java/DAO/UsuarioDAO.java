2284011747c9b0e795d8bd4bbe9d14c1fcd2771f
Henrique Santana <henrique.santana@injunior.com.br>
2023-11-20 21:00:12 -0300

    feat (UsuarioDAO): método de listar todos os usuarios

--- src/main/java/DAO/UsuarioDAO.java
+++ src/main/java/DAO/UsuarioDAO.java
@@ -2,12 +2,16 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Usuario;
 
 
 public class UsuarioDAO {
 	private String sqlCreate = "INSERT INTO Usuarios(nome, sobrenome, email, cpf, senha) VALUES (?, ?, ?, ?, ?)";
+	private String sqlAll = "Select * from Usuarios";
 	
 	public UsuarioDAO() {
 	}
@@ -28,11 +32,44 @@ public class UsuarioDAO {
 			pstm.setString(5, usuario.getSenha());
 
 
-			System.out.println(pstm.executeUpdate());
+			pstm.executeUpdate();
 			return true;
 		} catch (Exception e) {
 			System.out.println(e);
 			return false;
+		} finally {
+			db.closeConexao();
+		}
+	}
+	
+	public List<Usuario> all() {
+		Conexao db = new Conexao();
+		Connection con = db.getConexao();
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+
+		PreparedStatement pstm = null;
+		ResultSet res = null;
+
+		try {
+			pstm = con.prepareStatement(sqlAll);
+			res = pstm.executeQuery();
+			
+			while(res.next()) {
+				Usuario user = new Usuario();
+				user.setNome(res.getString("nome"));
+				user.setSobrenome(res.getString("sobrenome"));
+				user.setEmail(res.getString("email"));
+				user.setTipo(res.getInt("tipo"));
+				user.setCpf(res.getString("cpf"));
+				user.setSenha(res.getString("senha"));
+				
+				usuarios.add(user);
+			}
+			
+			return usuarios;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
 		} finally {
 			db.closeConexao();
 		}
